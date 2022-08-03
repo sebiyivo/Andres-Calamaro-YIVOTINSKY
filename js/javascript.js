dac7b31d31c7b7557b640d196c462df380c5f59e
sebiyivo <sebiyivo@gmail.com>
2022-08-03 17:32:47 -0300

    Comienzo del diseño de la pagina TIENDA

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,129 @@
-const calcularEdadPromedio = (aniosTotales , cantPersonas) => aniosTotales / cantPersonas
-let edadIngresada
-let personas = 0
-let anios = 0
-
-function sumarEdades (anio) {
-   
-        anios += anio
-        personas++
-    
-}
-
-while (edadIngresada != "esc") {
-
-    edadIngresada = prompt("Ingresa una edad")
-    
-    if (edadIngresada != "esc") {
-        
-        edadNumerica = parseInt(edadIngresada)
-        sumarEdades(edadNumerica)
-        
+class Disco {
+    constructor(nombre, precio) {
+        this.nombre = nombre.toUpperCase()
+        this.precio = this.precioConIva()
+        this.vendido = false
     }
-   
+
+    precioConIva() {
+        let precioConIva = parseFloat(precio) * 1.21
+        return precioConIva
+    }
+
+    // comprar() {
+    //     this.vendido = true
+    // }
 }
 
-console.log(anios);
-console.log(personas);
+class Carrito {
+    constructor() {
+        this.valorTotal = 0
+        this.cantidadItems = 0
+        this.discos = []
+    }
+
+    agregarAlCarrito (Disco) {
+        this.valorTotal += Disco.precio
+        this.cantidadItems++
+        this.discos.push(Disco)
+    }
+
+    comprar () {
+        for (const disco of discos) {
+            disco.vendido = true
+        }
+        this.valorTotal = 0
+        this.cantidadItems = 0
+        this.discos = []
+    }
+    
+}
 
 
-alert("El promedio de edades es " + calcularEdadPromedio(anios,personas))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const calcularEdadPromedio = (aniosTotales , cantPersonas) => aniosTotales / cantPersonas
+// let edadIngresada
+// let personas = 0
+// let anios = 0
+
+// function sumarEdades (anio) {
+   
+//         anios += anio
+//         personas++
+    
+// }
+
+// while (edadIngresada != "esc") {
+
+//     edadIngresada = prompt("Ingresa una edad")
+    
+//     if (edadIngresada != "esc") {
+        
+//         edadNumerica = parseInt(edadIngresada)
+//         sumarEdades(edadNumerica)
+        
+//     }
+   
+// }
+
+// console.log(anios);
+// console.log(personas);
+
+
+// alert("El promedio de edades es " + calcularEdadPromedio(anios,personas))
