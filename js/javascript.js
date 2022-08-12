c24d17b83421b21d84e993c9d9f2ab58714350bc
sebiyivo <sebiyivo@gmail.com>
2022-08-12 11:28:01 -0300

    Implementacion JS pagina tienda. Respuesta boton agregar y numero carrito

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,59 @@
 class Disco {
-    constructor(nombre, precio) {
-        this.nombre = nombre.toUpperCase()
-        this.precio = precio * 1.21
-        this.vendido = false
+    constructor(imagen, nombre, precio) {
+        this.imagen = imagen
+        this.nombre = nombre
+        this.precio = precio
+  
     }
 
 }
+
+// const discos = []
+// const agregarAlArray = () => {
+
+//     const imagenDisco = document.getElementsByClassName("card-img-top")
+//     const nombreDisco = document.getElementsByClassName("card-img-top".title)
+//     const precioDisco = document.getElementsByClassName("card-price")
+
+//    while (imagen != "") {
+//      let i = 0
+     
+//      if (imagen != "") {
+//         const imagen = imagenDisco[i].src
+//     const nombre = imagenDisco[i].title
+//     const precio = precioDisco[i]
+
+//     discos.push(new Disco(imagen,nombre,precio)) 
+
+//      i++
+
+//      }
+    
+//    }
+
+// }
+
+// agregarAlArray()
+// console.log(discos);
+
+
+const botonesAgregar = document.getElementsByClassName("botonAgregar")
+const iconoCantidadCarrito = document.getElementById("iconoCantidadCarrito")
+
+for (let i = 0; i < botonesAgregar.length; i++) {
+   
+    const boton = botonesAgregar[i];
+
+    boton.onclick = () => {
+        boton.value = "AGREGADO"
+        boton.className += " botonAgregado"
+        let numeroCarrito = parseInt(iconoCantidadCarrito.innerHTML) + 1
+        iconoCantidadCarrito.innerHTML = numeroCarrito
+    }
+
+}
+
+
 
 class Carrito {
     constructor() {
@@ -14,7 +62,7 @@ class Carrito {
         this.discos = []
     }
 
-    agregarAlCarrito (Disco) {
+    agregarAlCarrito_ (Disco) {
  
         if (!this.discos.includes(Disco)) {
 
@@ -54,54 +102,30 @@ class Carrito {
 }
 
 
+
+
+
+
+
 // /////////////////////////////////////////
 
-const nombre1 = prompt("Ingresa nombre del primer disco")
-const precio1 = parseInt(prompt("Ingresa precio del primer disco")) 
-const nombre2 = prompt("Ingresa nombre del segundo disco")
-const precio2 = parseInt(prompt("Ingresa precio del segundo disco")) 
-const nombre3 = prompt("Ingresa nombre del tercer disco")
-const precio3 = parseInt(prompt("Ingresa precio del tercer disco")) 
+// const nombre1 = prompt("Ingresa nombre del primer disco")
+// const precio1 = parseInt(prompt("Ingresa precio del primer disco")) 
+// const nombre2 = prompt("Ingresa nombre del segundo disco")
+// const precio2 = parseInt(prompt("Ingresa precio del segundo disco")) 
+// const nombre3 = prompt("Ingresa nombre del tercer disco")
+// const precio3 = parseInt(prompt("Ingresa precio del tercer disco")) 
 
-const disco1 = new Disco(nombre1,precio1);
-const disco2 = new Disco(nombre2,precio2);
-const disco3 = new Disco(nombre3,precio3);
+// const disco1 = new Disco(nombre1,precio1);
+// const disco2 = new Disco(nombre2,precio2);
+// const disco3 = new Disco(nombre3,precio3);
 
-const carrito = new Carrito
-carrito.agregarAlCarrito(disco1)
-carrito.agregarAlCarrito(disco2)
-carrito.agregarAlCarrito(disco3)
+// const carrito = new Carrito
+// carrito.agregarAlCarrito(disco1)
+// carrito.agregarAlCarrito(disco2)
+// carrito.agregarAlCarrito(disco3)
 
-alert("EL VALOR TOTAL POR LOS 3 DISCOS ES $" + carrito.valorTotal)
-
-
-// const calcularEdadPromedio = (aniosTotales , cantPersonas) => aniosTotales / cantPersonas
-// let edadIngresada
-// let personas = 0
-// let anios = 0
-
-// function sumarEdades (anio) {
-   
-//         anios += anio
-//         personas++
-    
-// }
-
-// while (edadIngresada != "esc") {
-
-//     edadIngresada = prompt("Ingresa una edad")
-    
-//     if (edadIngresada != "esc") {
-        
-//         edadNumerica = parseInt(edadIngresada)
-//         sumarEdades(edadNumerica)
-        
-//     }
-   
-// }
-
-// console.log(anios);
-// console.log(personas);
+// alert("EL VALOR TOTAL POR LOS 3 DISCOS ES $" + carrito.valorTotal)
 
 
-// alert("El promedio de edades es " + calcularEdadPromedio(anios,personas))
+
